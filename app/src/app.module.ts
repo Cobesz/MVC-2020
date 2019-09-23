0e80aea83957551248ecce0a82affe84e0b41735
Cas <cas@vorteq.nl>
2019-09-23 14:57:00 +0200

    Router works

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -6,14 +6,33 @@ import { UsersModule } from './users/users.module';
 import * as process from 'process';
 import {ConfigService} from './config/config.service';
 import {ConfigModule} from './config/config.module';
+import {RouterModule, Routes} from "nest-router";
 
 const configService = new ConfigService(`${process.env.NODE_ENV}.env`);
 
 const username = configService.get('DATABASE_USER') || 'mysql';
 const password = configService.get('DATABASE_PASSWORD') || 'example';
 
+const routes: Routes = [
+    {
+        path: '/users',
+        module: UsersModule,
+        // children: [
+        //     {
+                // path: '/cats',
+                // module: CatsModule,
+            // },
+            // {
+                // path: '/dogs',
+                // module: DogsModule,
+        //     },
+        // ],
+    },
+];
+
 @Module({
     imports: [
+        RouterModule.forRoutes(routes),
         TypeOrmModule.forRoot({
             type: 'mysql',
             host: 'localhost',
