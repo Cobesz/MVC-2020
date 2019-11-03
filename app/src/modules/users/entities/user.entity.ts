2ed94adb7da5047f75b92779455988b53aa490fc
Cas <cas@vorteq.nl>
2019-11-03 15:17:52 +0100

    added validators to the user model

--- app/src/modules/users/entities/user.entity.ts
+++ app/src/modules/users/entities/user.entity.ts
@@ -1,24 +1,39 @@
 import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from 'typeorm';
 import {WishList} from '../../wishlists/entities/wishlist.entity';
+import {IsBoolean, IsEmail, IsInt, IsString} from 'class-validator';
 
 @Entity()
 export class User {
+
     @PrimaryGeneratedColumn()
     id: number;
+
+    @IsString({ always: true }) // validator
     @Column({unique: false})
     firstName: string;
+
+    @IsString({ always: true }) // validator
     @Column({unique: false})
     middleName: string;
+
+    @IsString() // validator
     @Column({nullable: false})
     lastName: string;
+
+    @IsEmail() // validator
     @Column({nullable: true})
     email: string;
-    @Column({ length: 100, nullable: true })
-    password: string|undefined;
-    @Column({ length: 100, nullable: true })
-    passwordHash: string|undefined;
+
+    @Column({length: 100, nullable: true})
+    password: string | undefined;
+
+    @Column({length: 100, nullable: true})
+    passwordHash: string | undefined;
+
+    @IsBoolean() // validator
     @Column({unique: false})
     admin: boolean;
+
     @OneToMany(type => WishList, wishlist => wishlist.user)
     wishlists: WishList[];
 }
